f5300a97fc5003b4e0cd3245abf496416c784e20
isomorphic-not <abkgit@gmail.com>
2021-01-10 11:39:06 -0500

    add carousel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ class Footer extends React.Component {
     render() {
       return <footer class="footer">
       <div class="content has-text-centered">
-        <p>Hosted with <a href="https://www.github.com">Github</a>.</p>
-        <p>TLS certs pending through <a href="https://letsencrypt.org">LetsEncrypt</a>.</p>
+        <p>Hosted and certified by <a href="https://www.github.com">Github</a>.</p>
         <p>Maintained by <a href="https://www.github.com/isomorphic-not">Isomorphic-not</a>.</p>
       </div>
     </footer>
