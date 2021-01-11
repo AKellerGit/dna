d9c40dcf5619264fd5246e0232929687bb16a968
isomorphic-not <abkgit@gmail.com>
2021-01-11 14:24:38 -0500

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <img src="dna.png" width="250" onDragStart={handleDragStart} className="center-carousel" />,
+  <img src="stock2.png" onDragStart={handleDragStart} className="center-carousel" />,
+  <img src="stock3.png" onDragStart={handleDragStart} className="center-carousel" />,
+];
+
 
 class App extends React.Component {
   render() {
       return (
-<       div>test</div>
-      )  
+        <AliceCarousel 
+        disableDotsControls="true"
+        disableButtonsControls="true"
+        className="center-carousel" 
+        autoPlay="true" 
+        infinite="true" 
+        autoPlayInterval="4000" 
+        items={items} 
+        />
+      )
   }
 }
 export default App;
-
-
